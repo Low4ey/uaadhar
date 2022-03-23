5eaf95cc49c8a3d76b4d6f1eda5b5add76d290ba
Ajeet Kumar <ajeet8364@gmail.com>
2022-03-23 22:38:47 +0530

    1.03
    minor change

--- src/screens/userLogin.js
+++ src/screens/userLogin.js
@@ -1,10 +1,15 @@
 import { Text, StyleSheet, View , Image} from 'react-native'
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Btn from '../utils/button.js'
 import Input from '../utils/input.js'
 const st=require( "../styles/loginst.js")
 
 export default function Login(){
+  const [isAadhar,setAadhar]=useState(false)
+  const [btn,setBtn]=useState("Get OTP")
+  const onClick=()=>{
+    
+  }
     return (
       <View>
       <Image
@@ -13,7 +18,7 @@ export default function Login(){
         <Text>login</Text>
         <Input name="Aadhar No"/>
         <Btn 
-        title="Get OTP" />
+        title={btn} />
       </View>
     )
   }
